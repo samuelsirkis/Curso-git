b814326b68ae3ded6dea62e23b45a60e7ab92d6b
Samuel Sirkis <samuelsirkis@yahoo.com>
2019-06-11 12:28:48 -0300

    Acrecentado pergunta como Vc esta e logica for

--- CourseAppGitGithub/Program.cs
+++ CourseAppGitGithub/Program.cs
@@ -8,6 +8,12 @@ namespace CourseAppGitGithub
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning!");
+            Console.WriteLine("Como Você Está Hoje?");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("oi");
+            }
 
             Console.ReadLine();
         }
