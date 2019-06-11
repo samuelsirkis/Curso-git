f551191bf6cf908e9841e6cadd574cc64d7a9e35
Samuel Sirkis <samuelsirkis@yahoo.com>
2019-06-11 10:57:38 -0300

    acrescentado mensagem Bom Dia

--- CourseAppGitGithub/Program.cs
+++ CourseAppGitGithub/Program.cs
@@ -7,6 +7,7 @@ namespace CourseAppGitGithub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning!");
 
             Console.ReadLine();
         }
